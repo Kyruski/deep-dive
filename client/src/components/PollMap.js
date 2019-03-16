ea02976033315b982db5593876467260afb1c813
noahspann <43322852+noahspann@users.noreply.github.com>
2019-03-16 17:14:22 -0500

    sets zoom to 12

--- client/src/components/PollMap.js
+++ client/src/components/PollMap.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
 import zipcodes from 'zipcodes';
+import { format } from 'url';
 class PollMap extends Component {
     constructor(props) {
         super(props)
@@ -10,36 +11,54 @@ class PollMap extends Component {
               height: 400,
               latitude: 37.7577,
               longitude: -122.4376,
-              zoom: 8
-            }
+              zoom: 12
+            },
+            input: ''
           };
           this.dummyData = 78665
-          this.handleLocation = this.handleLocation.bind(this)
+          this.handleLocation = this.handleLocation.bind(this);
+          this.handleInput = this.handleInput.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this)
         }
-      
-        handleLocation () {
-            let location = zipcodes.lookup(this.dummyData);
+        handleInput(e) {
+            this.setState({
+                input: e.target.value
+            })
+        }
+        
+        handleSubmit(e) {
+            e.preventDefault();
+            this.handleLocation(this.state.input)
+        }
+        
+        handleLocation (zipcode) {
+            let location = zipcodes.lookup(zipcode);
             this.setState({
                 viewport:{
                     width: 400,
                     height: 400,
                     latitude: location.latitude,
                     longitude: location.longitude,
-                    zoom: 8
+                    zoom: 11
                 }
             })
         }
         componentDidMount() {
-            this.handleLocation()
+            this.handleLocation(75605)
             
         }
     render() {
         return (
-          <ReactMapGL
-            {...this.state.viewport}
-            onViewportChange={(viewport) => this.setState({viewport})}
-            mapboxApiAccessToken={'pk.eyJ1IjoiZGV2bm9haCIsImEiOiJjanRieGNpZzAwcW5tNDRyeXhvbG5tZjZjIn0.tB7Rsz3NKcNnpwfWWMltVg'}
-          />
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleInput} type="text"></input>
+            </form>
+            <ReactMapGL
+                {...this.state.viewport}
+                onViewportChange={(viewport) => this.setState({viewport})}
+                mapboxApiAccessToken={'pk.eyJ1IjoiZGV2bm9haCIsImEiOiJjanRieGNpZzAwcW5tNDRyeXhvbG5tZjZjIn0.tB7Rsz3NKcNnpwfWWMltVg'}
+            />
+        </div>
         );
     }
 }
