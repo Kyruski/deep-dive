e927269bdea111bcdf706cd00f214e47e1c16200
Laura <laura.robertson64@gmail.com>
2019-03-16 21:48:52 -0500

    chore(cheerio): tinkering with cheerio

--- server/voterid/potusScraper.js
+++ server/voterid/potusScraper.js
@@ -1,13 +1,13 @@
 
 const rp = require('request-promise');
-const cheerio = require('cheerio');
+const $ = require('cheerio');
 const url = 'https://www.vote.org/voter-id-laws/';
 
 rp(url)
   .then(function(html){
     //success!
-    const $ = cheerio.load(html);
-    console.log(html);
+    console.log($('state-row > td', html).length);
+    // console.log($('state-row > td', html));
   })
   .catch(function(err){
     //handle error
