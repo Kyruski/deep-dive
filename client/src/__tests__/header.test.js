5b866e84a67b81a6bbb26ba77bd3b5e10a001fd9
Dan Kaiser <Tophatman100@gmail.com>
2019-03-19 14:46:16 -0500

    test(header.test.js): Made a simple test to see if component is on the page
    #62

---
+++ client/src/__tests__/header.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render, cleanup, configure } from 'react-testing-library'
+
+// this adds custom jest matchers from jest-dom
+import 'jest-dom/extend-expect'
+import Header from '../header';
+
+afterEach(cleanup);
+configure({ testIdAttribute: 'test-id' });
+
+const setup = () => {
+  const { getByTestId } = render(<Header />);
+  const form = getByTestId('header');
+  return { form };
+}
+
+it('header is on the page', () => {
+  const form = setup();
+  expect(form).toBeTruthy();
+});
