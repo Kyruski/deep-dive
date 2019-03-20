8a5f0fb209220e33d7484b2944550b6ad605f8f6
Laura <laura.robertson64@gmail.com>
2019-03-20 10:16:32 -0500

    chore(clean-up): minor changes before master pull
    #8

--- server/routes/voterId.js
+++ server/routes/voterId.js
@@ -1,18 +1,13 @@
 const router = require('express').Router();
-// const bodyParser = require('body-parser');
 const db = require('../../db/index.js')
 
-// let urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-router.get('/:state', urlencodedParser, (req, res) => {
-  console.log('what is this --->', req.params.state);
+router.get('/:state', (req, res) => {
   let state = req.params.state;
   db.getStateIdLaws(state, (err, results) => {
     if (err) {
       console.log('error on voterId API: ', err)
       res.end()
     } else {
-      console.log('our results', results)
       res.send(results)
     }
   })
