45888e1ba4ea7f1a1bbaa3d54f417a0660992205
Sarah <sarahrva@gmail.com>
2019-03-20 15:09:19 -0500

    feat(modified: client/package-lock.json, client/package.json, client/src/App.js, client/src/componen
    Add in-person, absentee voting information cards t
    
    #8

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
-import VoterInfoDetail from './components/VoterInfoDetail.js'
+import React, { Component } from "react";
+import "./App.css";
+import VoterInfoDetail from "./components/VoterInfoDetail.js";
+import StateId from "./components/stateId";
 
-const api = `http://localhost:8000/api/example`
-
+const api = `http://localhost:8000/api/example`;
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = { seaCreatures: [] }
+  constructor() {
+    super();
+    this.state = { seaCreatures: [] };
   }
 
-  componentDidMount() {
-    fetch(api)
-      .then(res => res.json())
-      .then(seaCreatures => {
-        this.setState({ seaCreatures: seaCreatures.data })
-      })
-  }
+  // componentDidMount() {
+  //   fetch(api)
+  //     .then(res => res.json())
+  //     .then(seaCreatures => {
+  //       this.setState({ seaCreatures: seaCreatures.data });
+  //     });
+  // }
 
   render() {
     return (
       <>
         <h1>Welcome to Blue Ocean</h1>
         <ul>
-          {this.state.seaCreatures.map((seaCreature, i) => <li key={i}>{seaCreature}</li>)}
+          {this.state.seaCreatures.map((seaCreature, i) => (
+            <li key={i}>{seaCreature}</li>
+          ))}
         </ul>
         <VoterInfoDetail />
+        <StateId />
       </>
     );
   }
---
+++ client/src/components/stateId.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import "antd/dist/antd.css";
+import { Card, Col, Row } from "antd";
+import axios from "axios";
+
+const myStyles = {
+  height: "500px"
+};
+
+const StateId = props => {
+  let [usaState, setUsaState] = useState("Select a state");
+  let [inPerson, setInPerson] = useState("In person details here");
+  let [absentee, setAbsentee] = useState("Absentee details here");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/voter", {
+        params: {
+          id: ""
+        }
+      })
+      .then(response => {
+        // console.log("use effect response", response.data);
+        setUsaState(response.data[0].state);
+        setInPerson(response.data[0].in_person);
+        setAbsentee(response.data[0].absentee);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+  return (
+    <div style={{ background: "#ECECEC", padding: "30px" }}>
+      <h1>{usaState}</h1>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Card title="In-person Voting" bordered={false} style={myStyles}>
+            <p>{inPerson}</p>
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title="Absentee Voting" bordered={false} style={myStyles}>
+            <p>{absentee}</p>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default StateId;
