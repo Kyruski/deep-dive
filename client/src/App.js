33ae6c1752a4788838723ef241de6f9127123de6
Laura <laura.robertson64@gmail.com>
2019-03-20 17:00:47 -0500

    fix(conflicts): added app changes to merge conflicts
    #8

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import VoterInfoDetail from "./components/VoterInfoDetail.js";
-import { Router, Link } from "@reach/router";
-import LandingPage from "./components/LandingPage";
-import { Head, HeadRoutes } from "./header";
-import './App.css';
-import VoterId from './components/voterId';
-=======
 import './App.css';
 import { Head, MainRoutes } from './components/header'
 
->>>>>>> 2480b26235984a466aabfb3c47490f3911aa22ac
 
 console.log(MainRoutes);
-const api = `http://localhost:8000/api/example`;
 
 class App extends Component {
   constructor() {
@@ -27,19 +17,8 @@ class App extends Component {
     return (
       <>
         <Head />
-<<<<<<< HEAD
-        <HeadRoutes />
-        <VoterInfoDetail />
-        <Router>
-          <LandingPage path="/" />
-          <CandidateList path="candidates/*" />
-          <VoterInfoDetail path="voterInfo/*" />
-        </Router>
-        <VoterId />
-=======
         <MainRoutes />
         {/* need to update with CandidateList */}
->>>>>>> 2480b26235984a466aabfb3c47490f3911aa22ac
       </>
     );
   }
