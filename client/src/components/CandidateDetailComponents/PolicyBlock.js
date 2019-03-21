0b17e057bff4207eb0631a1af0dfc0dab5bcf14a
Scott Elder <smelder@gmail.com>
2019-03-20 19:17:56 -0500

    feat(CandidateDetail): Add Candidate Detail Page and Policy Block.
    ref #25

--- client/src/components/CandidateDetailComponents/PolicyBlock.js
+++ client/src/components/CandidateDetailComponents/PolicyBlock.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PolicyCard from './PolicyCard.js';
 
 const PolicyBlock = (props) => {
-  const { abortion, lgbtAdoption, gunControl, equalPay, climateChange, gayMarriage, borderWall, plannedParenthood } = props.policies;
-  const policies = [abortion, lgbtAdoption, gunControl, equalPay, climateChange, gayMarriage, borderWall, plannedParenthood];
+  const { abortion, lgbtAdoption, gunControl, equalPay, climateChange, gayMarriage, borderWall, plannedParrenthood } = props.policies; //sic
+  const policies = [abortion, lgbtAdoption, gunControl, equalPay, climateChange, gayMarriage, borderWall, plannedParrenthood];
   const policyNames = ['Adoption', 'LGBT Adoption', 'Gun Control', 'Equal Pay', 'Climate Change', 'Gay Marriage', 'Border Wall', 'Planned Parenthood'];
   
   const divStyle = {
