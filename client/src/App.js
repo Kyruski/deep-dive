ef320a2334bf8fce12cfb5399357a0d671d9cdfa
Trevor Smith <smittrevor@gmail.com>
2019-03-20 19:06:06 -0500

    Serving static bundled index.html

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
+import PollMap from "./components/PollMap.js";
 import "./App.css";
-import { Head, HeadRoutes } from "./header";
-import VoterInfoDetail from "./components/VoterInfoDetail.js";
-import CandidateCards from "./components/CandidateCards.js";
-
-import { Router, Link } from "@reach/router";
-import LandingPage from "./components/LandingPage";
-
-=======
-
-import React, { Component } from 'react';
-import PollMap from './components/PollMap.js';
-import './App.css';
-import { Head, MainRoutes } from './components/header'
-
+import { Head, MainRoutes } from "./components/header";
 
 console.log(MainRoutes);
->>>>>>> 060dc5226ecd21cd439fdd77ae7cd6a701c885ff
 const api = `http://localhost:8000/api/example`;
 
 class App extends Component {
@@ -26,24 +12,14 @@ class App extends Component {
     this.state = { seaCreatures: [] };
   }
 
-  componentDidMount() { }
+  componentDidMount() {}
 
   render() {
     return (
       <>
         <Head />
-<<<<<<< HEAD
-        <HeadRoutes />
-        <VoterInfoDetail />
-        <Router>
-          <LandingPage path="/" />
-          <CandidateList path="candidates/*" />
-          <VoterInfoDetail path="voterInfo/*" />
-        </Router>
-=======
         <MainRoutes />
         {/* need to update with CandidateList */}
->>>>>>> 060dc5226ecd21cd439fdd77ae7cd6a701c885ff
       </>
     );
   }
