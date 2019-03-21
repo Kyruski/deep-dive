ce7ee54c131f9b1bd5664733f352ded7f91bf239
Scott Elder <smelder@gmail.com>
2019-03-20 19:29:31 -0500

    feat(CandidateDetail): Add CandidateDetail and Policy Block
    ref #25

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,25 @@
-
-import React, { Component } from 'react';
-import PollMap from './components/PollMap.js';
-import './App.css';
+import React, { Component } from "react";
+import PollMap from "./components/PollMap.js";
 import CandidateDetail from './components/CandidateDetail.js'
-import { Head, MainRoutes } from './components/header'
-
+import "./App.css";
+import { Head, MainRoutes } from "./components/header";
 
 console.log(MainRoutes);
 const api = `http://localhost:8000/api/example`;
 
 class App extends Component {
   constructor() {
-
     super();
     this.state = { seaCreatures: [] };
   }
 
-  componentDidMount() { }
+  componentDidMount() {}
 
   render() {
     return (
       <>
-        <h1>Welcome to Blue Ocean</h1>
-        <ul>
-          {/* {this.state.seaCreatures.map((seaCreature, i) => <li key={i}>{seaCreature}</li>)} */}
-        </ul>
-        {/* <VoterInfoDetail /> */} 
-        <CandidateDetail />
-        {/* <Head /> */}
-        {/* <MainRoutes /> */}
+        <Head />
+        <MainRoutes />
         {/* need to update with CandidateList */}
       </>
     );
